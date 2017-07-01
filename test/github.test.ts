2afa649637350cdaa696fcc2902f6de776c30755
Junle Li <lijunle@gmail.com>
2017-07-01 15:27:13 +0800

    Fix failing test cases. (#3)
    * Generate package-lock.json file.
    
    * Fix the failing test after rename.
    
    * Replace every `gitlinks` to `git-urls`.

--- test/github.test.ts
+++ test/github.test.ts
@@ -4,13 +4,12 @@ import GitUrl from "../src/index";
 
 test("Get current project's git online link", async () => {
     const link = await GitUrl.getOnlineLinkAsync(path.resolve("package.json"));
-
-    expect(link).toBe("https://github.com/qinezh/gitlink/blob/dev/package.json");
+    expect(link).toMatch(/https:\/\/github.com\/.+\/git-urls\/blob\/.+\/package.json/);
 });
 
 test("Get HTTPS URL in GitHub", async () => {
     const configInfo = {
-        remoteUrl: "https://github.com/build/gitlink.git",
+        remoteUrl: "https://github.com/build/git-urls.git",
         branchName: "master",
         startLine: undefined,
         endLine: undefined,
@@ -18,12 +17,12 @@ test("Get HTTPS URL in GitHub", async () => {
     }
     const link = await GitUrl.getOnlineLinkCoreAsync(configInfo);
 
-    expect(link).toBe("https://github.com/build/gitlink/blob/master/test/a.md");
+    expect(link).toBe("https://github.com/build/git-urls/blob/master/test/a.md");
 });
 
 test("Get SSH URL in GitHub", async () => {
     const configInfo = {
-        remoteUrl: "git@github.com:qinezh/gitlink",
+        remoteUrl: "git@github.com:qinezh/git-urls",
         branchName: "master",
         startLine: undefined,
         endLine: undefined,
@@ -31,12 +30,12 @@ test("Get SSH URL in GitHub", async () => {
     }
     const link = await GitUrl.getOnlineLinkCoreAsync(configInfo);
 
-    expect(link).toBe("https://github.com/qinezh/gitlink/blob/master/test/a.md");
+    expect(link).toBe("https://github.com/qinezh/git-urls/blob/master/test/a.md");
 });
 
 test("Get HTTP URL in GitHub", async () => {
     const configInfo = {
-        remoteUrl: "http://github.com/qinezh/gitlink.git",
+        remoteUrl: "http://github.com/qinezh/git-urls.git",
         branchName: "master",
         startLine: undefined,
         endLine: undefined,
@@ -44,12 +43,12 @@ test("Get HTTP URL in GitHub", async () => {
     }
     const link = await GitUrl.getOnlineLinkCoreAsync(configInfo);
 
-    expect(link).toBe("http://github.com/qinezh/gitlink/blob/master/test/a.md");
+    expect(link).toBe("http://github.com/qinezh/git-urls/blob/master/test/a.md");
 });
 
 test("Get HTTPS URL with username in GitHub", async () => {
     const configInfo = {
-        remoteUrl: "https://qinezh@github.com/build/gitlink.git",
+        remoteUrl: "https://qinezh@github.com/build/git-urls.git",
         branchName: "master",
         startLine: undefined,
         endLine: undefined,
@@ -57,5 +56,5 @@ test("Get HTTPS URL with username in GitHub", async () => {
     }
     const link = await GitUrl.getOnlineLinkCoreAsync(configInfo);
 
-    expect(link).toBe("https://github.com/build/gitlink/blob/master/test/a.md");
+    expect(link).toBe("https://github.com/build/git-urls/blob/master/test/a.md");
 });
