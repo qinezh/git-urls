7b9bf30cf38f80cd3a12a670b8961a1586adb1dc
Qinen Zhu <qinzhu@microsoft.com>
2017-09-10 15:46:03 +0800

    Fix bug of line number in bitbucket

--- src/host/bitbucket.ts
+++ src/host/bitbucket.ts
@@ -1,5 +1,29 @@
+import * as path from "path";
+
 import BasicHost from "./basicHost";
+import GitInfo from "../gitInfo";
 
 export default class BitBucket extends BasicHost {
     protected separateFolder = "src";
+
+    assemble(info: GitInfo): string {
+        let prefix = "https://";
+        if (info.metadata && info.metadata["isHttp"]) {
+            prefix = "http://";
+        }
+        const link = `${prefix}${info.hostName}/${info.userName}/${info.repoName}/${this.separateFolder}/${info.branchName}/${info.relativefilePath}`
+
+        let fileName: string|null = null;
+        if (info.relativefilePath) {
+            fileName = path.basename(info.relativefilePath);
+        }
+
+        if (info.section && info.section.startLine && info.section.endLine && info.section.startLine !== info.section.endLine && fileName) {
+            return `${link}#${fileName}-${info.section.startLine}:${info.section.endLine}`;
+        } else if (info.section && info.section.startLine && fileName) {
+            return `${link}#${fileName}-${info.section.startLine}`;
+        } else {
+            return link;
+        }
+    }
 }
--- test/bitbucket.test.ts
+++ test/bitbucket.test.ts
@@ -17,9 +17,15 @@ test("Get SSH URL in BitBucket", async () => {
     const configInfo = {
         remoteUrl: "git@bitbucket.org:qinezh/git-urls.git",
         branchName: "master",
+        section: {
+            startLine: 2,
+            endLine: 3,
+            startColumn: 4,
+            endColumn: 5
+        },
         relativePath: "test/a.md"
     }
     const link = await GitUrls["getUrlAsync"](configInfo);
 
-    expect(link).toBe("https://bitbucket.org/qinezh/git-urls/src/master/test/a.md");
+    expect(link).toBe("https://bitbucket.org/qinezh/git-urls/src/master/test/a.md#a.md-2:3");
 });
